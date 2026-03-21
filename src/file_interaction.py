0866099d6757590d300a029d79a67b048589e827
Viktor Bukotin <viktorbukotin0@gmail.com>
2026-03-21 18:58:05 +0300

    vacancy_interaction_in _progress

--- src/file_interaction.py
+++ src/file_interaction.py
@@ -1,13 +1,7 @@
-import os
-from dotenv import load_dotenv
-
 import psycopg2
 
-load_dotenv()
-params = {'host': os.getenv('HOST'), 'user': os.getenv('USER'),
-        'password': os.getenv('PASSWORD'), 'port': os.getenv('PORT')}
 
-def create_database(new_database: str) -> None:
+def create_database(new_database: str, params: dict) -> None:
     """Создание новой БД"""
     try:
         conn = psycopg2.connect(database='postgres', **params)
@@ -22,48 +16,36 @@ def create_database(new_database: str) -> None:
         conn.close()
 
 
-def create_table(new_database: str) -> None:
+def create_table(conn) -> None:
     """Создание таблиц БД"""
-    try:
-        conn = psycopg2.connect(database=new_database, **params)
-        with conn.cursor() as cur:
-            cur.execute('''CREATE TABLE IF NOT EXISTS company(
-                        company_id INT PRIMARY KEY,
-                        company_name VARCHAR(100),
-                        company_url VARCHAR(100),
-                        count_vacancyes INT)'''
-                        )
-            cur.execute('''CREATE TABLE IF NOT EXISTS vacancy(
-                        vacancy_id SERIAL PRIMARY KEY,
-                        vacancy VARCHAR(100),
-                        vacancy_url VARCHAR(100),
-                        salary_from INT,
-                        salary_to INT,
-                        area VARCHAR(100),
-                        published_date DATE,
-                        company_id INT REFERENCES company(company_id))'''
-                        )
-            conn.commit()
-    except psycopg2.DatabaseError as e:
-        print(f"Ошибка при подключении к базе данных: {e}")
-        conn = None
-    if conn is not None:
-        conn.close()
+    with conn.cursor() as cur:
+        cur.execute('''CREATE TABLE IF NOT EXISTS company(
+                    company_id INT PRIMARY KEY,
+                    company_name VARCHAR(100),
+                    company_url VARCHAR(100),
+                    count_vacancyes INT)'''
+                    )
+        cur.execute('''CREATE TABLE IF NOT EXISTS vacancy(
+                    vacancy_id SERIAL PRIMARY KEY,
+                    vacancy VARCHAR(100),
+                    vacancy_url VARCHAR(100),
+                    salary_from INT,
+                    salary_to INT,
+                    area VARCHAR(100),
+                    published_date DATE,
+                    company_id INT REFERENCES company(company_id))'''
+                    )
+        conn.commit()
 
-def insert_table(new_database: str, list_data_employers: list[dict], list_data_vacancy: list[dict]) -> None:
+
+def insert_table(list_data_employers: list[dict], list_data_vacancy: list[dict], conn) -> None:
     """Заполнение таблиц, данными по работодателям и вакансиям"""
-    try:
-        conn = psycopg2.connect(database=new_database, **params)
-        with conn.cursor() as cur:
-            for i in list_data_employers:
-                cur.execute('INSERT INTO company(company_id, company_name, company_url, count_vacancyes) VALUES (%s, %s, %s, %s)',
-                            tuple(i.values()))
-            for i in list_data_vacancy:
-                cur.execute('''INSERT INTO vacancy(vacancy, vacancy_url, salary_from, salary_to, area, 
-                            published_date, company_id) VALUES (%s, %s, %s, %s, %s, %s, %s)''', tuple(i.values()))
-            conn.commit()
-    except psycopg2.DatabaseError as e:
-        print(f"Ошибка при подключении к базе данных: {e}")
-        conn = None
-    if conn is not None:
-        conn.close()
+    with conn.cursor() as cur:
+        for i in list_data_employers:
+            cur.execute('INSERT INTO company(company_id, company_name, company_url, count_vacancyes) VALUES (%s, %s, %s, %s)',
+                        tuple(i.values()))
+        for i in list_data_vacancy:
+            cur.execute('''INSERT INTO vacancy(vacancy, vacancy_url, salary_from, salary_to, area, 
+                        published_date, company_id) VALUES (%s, %s, %s, %s, %s, %s, %s)''', tuple(i.values()))
+        conn.commit()
+
---
+++ src/vacancy_interaction.py
@@ -0,0 +1,34 @@
+from typing import Any
+
+
+class DBManager:
+    """Класс для получения данных из БД"""
+    def __init__(self, conn: Any) -> None:
+        """Инициализация экземпляров класса 'DBManager'"""
+        self.__conn = conn
+        self.__cur = self.__conn.cursor()
+
+    def get_companies_and_vacancies_count(self) -> list[str]:
+        """Получение списка всех компаний и количества вакансий у каждой компании"""
+        self.__cur.execute('SELECT company_name, COUNT(vacancy.vacancy) FROM company LEFT JOIN vacancy USING (company_id) GROUP BY company_id')
+        result = self.__cur.fetchall()
+        return [f'{x[0]} - {x[1]} вакансий' for x in result]
+
+    def get_all_vacancies(self) -> list[str]:
+        """Получеение списка всех вакансий с указанием названия компании, названия вакансии, зарплаты и ссылки на вакансию"""
+        self.__cur.execute(
+            'SELECT company_name, vacancy, salary_from, vacancy_url FROM vacancy LEFT JOIN company USING (company_id)')
+        result = self.__cur.fetchall()
+        return [f'{x[0]}, должность: {x[1]}, зарплата: {x[2]}, ссылка на вакансию: {x[3]}' for x in result]
+
+    def get_avg_salary(self) -> str:
+        """Получение средней зарплаты по вакансиям"""
+        self.__cur.execute(
+            'SELECT AVG(salary_from) FROM vacancy WHERE salary_from IS NOT NULL')
+        result = self.__cur.fetchone()
+        return f'Средняя зарплата по вакансиям: {round(result[0])} руб.'
+
+    def get_vacancies_with_higher_salary(self):
+        pass
+    def get_vacancies_with_keyword(self):
+        pass
